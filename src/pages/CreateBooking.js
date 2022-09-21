f0c1f6ea9a305f19ec56b12a7aaa9d6c2a1c7d41
thaihoa1408 <phamthaihoa28@gmail.com>
2022-09-21 15:18:27 +0700

    update ui for create booking step 3

--- src/pages/CreateBooking.js
+++ src/pages/CreateBooking.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Accordion, Button } from 'react-bootstrap';
 import { ReactComponent as Logo } from '../assets/images/logo.svg';
 import { ReactComponent as WavingIcon } from '../assets/icons/waving.svg';
+import { ReactComponent as PhoneIcon } from '../assets/icons/phone.svg';
 import '../assets/styles/CreateBooking.scss';
 import { Steps } from 'antd';
 import { Avatar } from 'antd';
@@ -76,7 +77,7 @@ export default function CreateBooking() {
                 path: '/locations/details/1',
               },
               {
-                label: 'Thông tin đặt chỗ',
+                label: 'Tiến hành đặt chỗ',
                 active: true,
               },
             ]}
@@ -464,7 +465,70 @@ export default function CreateBooking() {
         )}
         {currentStep === 2 && (
           <div className='row'>
-            <div className='mx-auto col-xl-5 col-lg-7 info-booking'></div>
+            <div className='mx-auto col-xl-5 col-lg-7 info-booking'>
+              <div>
+                <div className='title'>Đơn đặt của bạn</div>
+                <div className='status-booking'>
+                  <div>Trạng thái chỗ đặt:</div>
+                  <div>
+                    <div className='status verify'>Đang xác thực</div>
+                  </div>
+                </div>
+                <div className='code-booking'>
+                  <div>Mã đơn đặt:</div>
+                  <div>#12345678</div>
+                </div>
+                <div className='confirm-booking'>
+                  <p>
+                    Email xác thực sẽ được gửi đến bạn sau khi hoàn tất quá
+                    trình xác thực. Chúng tôi sẽ tiến hành hủy đơn đặt chỗ nếu
+                    chưa nhận được xác nhận thanh toán sau:{' '}
+                  </p>
+                  <div>01:25:00</div>
+                </div>
+                <div className='location-info'>
+                  <div className='location-name'>circo đông du</div>
+                  <div>41 Đông Du, Bến Nghé, Quận 1, Thành Phố Hồ Chí Minh</div>
+                  <div className='workspace-item'>
+                    <img src={Img1} alt='' />
+                    <div>
+                      <div className='fw-bold'>x1 Bàn làm việc cá nhân</div>
+                      <div>Thứ Hai, 25 Tháng 2, 2022</div>
+                      <div>09:00 - 13:00 (4 tiếng)</div>
+                      <div>220.000đ</div>
+                    </div>
+                  </div>
+                </div>
+                <div className='summary'>
+                  <div className='title'>Tóm tắt thanh toán</div>
+                  <div className='price-detail'>
+                    <div>
+                      <span>Giá gốc</span>
+                      <span>250,000đ</span>
+                    </div>
+                    <div>
+                      <span>Giá sau giảm</span>
+                      <span>125,000đ</span>
+                    </div>
+                    <div>
+                      <span>Voucher</span>
+                      <span>-25,000đ</span>
+                    </div>
+                  </div>
+                  <div className='price-total'>
+                    <span>Tổng cộng</span>
+                    <span>100,000đ</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className='title'>Hỗ trợ</div>
+                <p>Gọi ngay cho chúng tôi ngay nếu bạn cần trợ giúp</p>
+                <div className='phone-button'>
+                  <PhoneIcon className='me-2' /> 1900 5232
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </div>
