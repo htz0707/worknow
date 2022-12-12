b76dfafdf74b8926ddd92ee4177ad8ea8337e94e
thaihoa1408 <phamthaihoa28@gmail.com>
2022-12-12 17:16:08 +0700

    handle create booking step 2

--- src/pages/BookingPayment.js
+++ src/pages/BookingPayment.js
@@ -3,18 +3,304 @@ import { Accordion } from 'react-bootstrap';
 import { ReactComponent as Logo } from '../assets/images/logo.svg';
 import { ReactComponent as WavingIcon } from '../assets/icons/waving.svg';
 import '../assets/styles/CreateBooking.scss';
-import { Steps } from 'antd';
-import Img1 from '../assets/images/location_img1.png';
+import { Steps, Upload } from 'antd';
 import MomoLogo from '../assets/images/momo.png';
 import VnpayLogo from '../assets/images/vnpay.jpg';
 import QrcodeImg from '../assets/images/qrcode.png';
 import { AiOutlineCheck } from 'react-icons/ai';
 import Bcrumb from '../components/Bcrumb';
 import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { gql, useLazyQuery, useMutation } from '@apollo/client';
+import {
+  formatCurrency,
+  handleMessage,
+  renderAddress,
+  returnTypeOfBooking,
+  toHoursAndMinutes,
+} from '../helpers/helpers';
+import ShowMore from '../components/ShowMore';
+import moment from 'moment';
+import Countdown from 'react-countdown';
+import axios from 'axios';
+import { ReactComponent as CountDownIcon } from '../assets/icons/CountDownClock.svg';
+import OrderExpireModal from '../components/OrderExpireModal';
 const { Step } = Steps;
 
 export default function BookingPayment() {
+  const { location_id, order_id } = useParams();
+  const navigate = useNavigate();
+  //get order details
+  const GET_ORDER_DETAILS = gql`
+    query GetOrderDetails($location_id: UUID!, $order_id: UUID!) {
+      location(id: $location_id) {
+        id
+        name
+        address
+        ward {
+          name
+        }
+        district {
+          name
+        }
+        city {
+          name
+        }
+        country {
+          name
+        }
+        images {
+          publicUrl
+        }
+        amenities {
+          name
+        }
+        closeTime
+        openTime
+      }
+      order(id: $order_id) {
+        email
+        fullname
+        bookingExpiredTime
+        id
+        finalTotal
+        note
+        companyName
+        phoneNumber
+        status
+        total
+        totalDiscount
+        phoneCountryCode
+        orderId
+        orderStatuses {
+          status
+        }
+        orderDetails {
+          endDate
+          hour
+          price
+          startDate
+          workingSpaces {
+            id
+            name
+            type
+            images {
+              publicUrl
+            }
+          }
+        }
+      }
+    }
+  `;
+  const [getOrderDetails, { called, refetch }] =
+    useLazyQuery(GET_ORDER_DETAILS);
+  const [locationInfo, setLocationInfo] = useState({});
+  const [workingSpaceInfo, setWorkingSpaceInfo] = useState({});
+  const [orderInfo, setOrderInfo] = useState({});
+  const handleGetOrderDetail = async () => {
+    if (location_id && order_id) {
+      if (!called) {
+        let res = await getOrderDetails({
+          variables: {
+            location_id: location_id,
+            order_id: order_id,
+          },
+        });
+        if (res.data) {
+          console.log(res.data);
+          setLocationInfo(res.data.location);
+          setWorkingSpaceInfo(res.data.order?.orderDetails[0]?.workingSpaces);
+          setOrderInfo(res.data.order);
+          if (res.data.order.status === 'booking_expired') {
+            setShowOrderExpire(true);
+          }
+        }
+      } else {
+        let res = await refetch({
+          variables: {
+            location_id: location_id,
+            order_id: order_id,
+          },
+        });
+        if (res.data) {
+          console.log(res.data);
+          setLocationInfo(res.data.location);
+          setWorkingSpaceInfo(res.data.order?.orderDetails[0]?.workingSpaces);
+          setOrderInfo(res.data.order);
+          if (res.data.order.status === 'booking_expired') {
+            setShowOrderExpire(true);
+          }
+        }
+      }
+    }
+  };
+  useEffect(() => {
+    handleGetOrderDetail();
+  }, [location_id, order_id]);
+  //
+  const [showMoreAmenities, setShowMoreAmenities] = useState(false);
   const [currentStep, setCurrentStep] = useState(1);
+  // handle upload image
+  const CREATE_UPLOAD_URL = gql`
+    mutation CreateUploadUrl($fileName: String!, $contentType: String!) {
+      createUploadUrl(
+        data: { fileName: $fileName, contentType: $contentType }
+      ) {
+        presignUrl
+        fileId
+      }
+    }
+  `;
+  const [createUploadUrl] = useMutation(CREATE_UPLOAD_URL);
+  const UPDATE_UPLOAD_URL = gql`
+    mutation UpdateUploadUrl($fileId: UUID!) {
+      updateUploadUrl(data: { fileId: $fileId }) {
+        publicUrl
+        status
+      }
+    }
+  `;
+  const [updateUploadUrl] = useMutation(UPDATE_UPLOAD_URL);
+  const [fileList, setFileList] = useState([]);
+  const customRequest = async (options) => {
+    try {
+      let file = options.file;
+      options.onProgress({ percent: 0 });
+      let res1 = await createUploadUrl({
+        variables: {
+          fileName: file?.name,
+          contentType: file?.type,
+        },
+      });
+      if (res1.data) {
+        var config = {
+          method: 'put',
+          url: res1.data.createUploadUrl.presignUrl,
+          headers: {
+            'Content-Type': file?.type,
+          },
+          data: file,
+        };
+        let res2 = await axios(config);
+        if (res2.statusText === 'OK') {
+          let res3 = await updateUploadUrl({
+            variables: {
+              fileId: res1.data.createUploadUrl.fileId,
+            },
+          });
+          options.onProgress({ percent: 100 });
+          options.onSuccess({
+            id: res1.data.createUploadUrl.fileId,
+            url: res3.data.updateUploadUrl.publicUrl,
+          });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      options.onError({ err });
+      handleMessage('error', 'Tải hóa đơn không thành công.');
+    }
+  };
+  const onRemove = (item) => {
+    console.log(item);
+  };
+  const handleUploadImage = (info) => {
+    setFileList(info.fileList);
+  };
+  //handle extend time payment
+  const UPDATE_ORDER = gql`
+    mutation UpdateOrder(
+      $orderId: UUID!
+      $isExtendBookingTimeExpire: Boolean!
+    ) {
+      updateOrder(
+        data: {
+          orderId: $orderId
+          isExtendBookingTimeExpire: $isExtendBookingTimeExpire
+        }
+      ) {
+        id
+      }
+    }
+  `;
+  const [updateOrder] = useMutation(UPDATE_ORDER);
+  const handleExtendTime = async () => {
+    try {
+      let res = await updateOrder({
+        variables: {
+          orderId: orderInfo.id,
+          isExtendBookingTimeExpire: true,
+        },
+      });
+      if (res.data) {
+        handleGetOrderDetail();
+      }
+    } catch (error) {
+      console.log(error);
+      handleMessage('error', 'Gia hạn không thành công.');
+    }
+  };
+  const checkExtendValid = (arr) => {
+    let filter = arr?.filter((item) => item.status === 'extended');
+    if (filter?.length < 2) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const renderCountDown = ({ minutes, seconds, completed }) => {
+    if (completed) {
+      setShowOrderExpire(true);
+      // Render a complete state
+      return <div>00:00 (Hết hạn thanh toán)</div>;
+    } else {
+      // Render a countdown
+      return (
+        <div className='d-flex align-items-center'>
+          <span>
+            {String(minutes)?.padStart(2, '0')}:
+            {String(seconds)?.padStart(2, '0')}
+          </span>
+          <button
+            className='btn-extend-time'
+            disabled={
+              minutes >= 3 || !checkExtendValid(orderInfo?.orderStatuses)
+            }
+            onClick={handleExtendTime}
+          >
+            <CountDownIcon /> Gia hạn
+          </button>
+        </div>
+      );
+    }
+  };
+  //
+  const [showOrderExpire, setShowOrderExpire] = useState(false);
+  // handle pay order with bank
+  const PAY_ORDER_WITH_BANK = gql`
+    mutation PayOrderWithBank($fileId: UUID!, $orderId: UUID!) {
+      payOrderWithBank(data: { fileId: $fileId, orderId: $orderId }) {
+        id
+      }
+    }
+  `;
+  const [payOrderWithBank] = useMutation(PAY_ORDER_WITH_BANK);
+  const handleFinish = async () => {
+    try {
+      let res = await payOrderWithBank({
+        variables: {
+          fileId: fileList[0]?.response?.id,
+          orderId: order_id,
+        },
+      });
+      if (res.data) {
+        navigate(`/create-booking/status/${location_id}/${orderInfo.id}`);
+      }
+    } catch (error) {
+      console.log(error);
+      handleMessage('error', 'Thanh toán không thành công.');
+    }
+  };
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -33,7 +319,7 @@ export default function BookingPayment() {
               },
               {
                 label: 'Thông tin văn phòng',
-                path: '/locations/details/1',
+                path: `/locations/${location_id}/working-space/${workingSpaceInfo.id}`,
               },
               {
                 label: 'Tiến hành đặt chỗ',
@@ -76,13 +362,18 @@ export default function BookingPayment() {
                 <div className='col-md-6'>
                   <div className='left'>
                     <div className='title'>Mã đơn đặt</div>
-                    <div className='content'>#123456</div>
+                    <div className='content'>#{orderInfo?.orderId}</div>
                   </div>
                 </div>
                 <div className='col-md-6'>
                   <div className='right'>
                     <div className='title'>Hết hạn thanh toán sau</div>
-                    <div className='content'>01:25:00</div>
+                    <div className='content'>
+                      <Countdown
+                        date={new Date(orderInfo.bookingExpiredTime)}
+                        renderer={renderCountDown}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -122,14 +413,13 @@ export default function BookingPayment() {
                           </div>
                           <div className='bank-amount'>
                             <span>Số tiền</span>
-                            <span>100,000đ</span>
+                            <span>{formatCurrency(orderInfo?.finalTotal)}</span>
                           </div>
                         </div>
                       </Accordion.Body>
                     </Accordion.Item>
                   </Accordion>
                 </div>
-                <div className='upload-bill'>Tải hóa đơn chuyển khoản</div>
                 <div className='fw-bold'>Ví điện tử</div>
                 <div className='according-ewallet'>
                   <Accordion>
@@ -171,7 +461,7 @@ export default function BookingPayment() {
                           </div>
                           <div className='amount'>
                             <span>Số tiền</span>
-                            <span>100,000đ</span>
+                            <span>{formatCurrency(orderInfo?.finalTotal)}</span>
                           </div>
                           <div className='qrcode'>
                             <div>Hoặc</div>
@@ -185,31 +475,53 @@ export default function BookingPayment() {
                     </Accordion.Item>
                   </Accordion>
                 </div>
+                <Upload
+                  listType='picture'
+                  defaultFileList={[...fileList]}
+                  customRequest={customRequest}
+                  onChange={handleUploadImage}
+                  onRemove={onRemove}
+                >
+                  <div className='upload-bill'>Tải hóa đơn chuyển khoản</div>
+                </Upload>
               </div>
             </div>
           </div>
           <div className='col-lg-5 info-workspace'>
             <div className='box-1'>
-              <img src={Img1} alt='' />
+              {locationInfo.images && (
+                <img src={locationInfo.images[0]?.publicUrl} alt='' />
+              )}
               <div>
-                <div>Circo Đông Du</div>
-                <div>41 Đông Du, Bến Nghé, Quận 1, TPHCM</div>
+                <div>{locationInfo?.name}</div>
+                <div>{renderAddress(locationInfo)}</div>
                 <div>{'4.8 / 5.0 Rất tốt (120 đánh giá)'}</div>
                 <ul>
-                  <li>
-                    <AiOutlineCheck style={{ color: '#04B000' }} /> Wifi miễn
-                    phí
-                  </li>
-                  <li>
-                    <AiOutlineCheck style={{ color: '#04B000' }} /> Đậu xe miễn
-                    phí
-                  </li>
-                  <li>
-                    <AiOutlineCheck style={{ color: '#04B000' }} /> Mini bar
-                  </li>
-                  <li>
-                    <AiOutlineCheck style={{ color: '#04B000' }} /> Máy in
-                  </li>
+                  {locationInfo?.amenities?.map((item, index) => {
+                    if (showMoreAmenities) {
+                      return (
+                        <li key={index}>
+                          <AiOutlineCheck style={{ color: '#04B000' }} />{' '}
+                          {item?.name}
+                        </li>
+                      );
+                    } else {
+                      if (index < 5) {
+                        return (
+                          <li key={index}>
+                            <AiOutlineCheck style={{ color: '#04B000' }} />{' '}
+                            {item?.name}
+                          </li>
+                        );
+                      }
+                    }
+                  })}
+                  {locationInfo?.amenities?.length > 10 && (
+                    <ShowMore
+                      show={showMoreAmenities}
+                      setShow={setShowMoreAmenities}
+                    />
+                  )}
                 </ul>
               </div>
             </div>
@@ -218,11 +530,49 @@ export default function BookingPayment() {
               <div>
                 <div className='selected-workspace'>
                   <span>x1</span>
-                  <img src={Img1} alt='' />
+                  {workingSpaceInfo.images && (
+                    <img src={workingSpaceInfo.images[0]?.publicUrl} alt='' />
+                  )}
                   <div>
-                    <div className='fw-bold'>Bàn làm việc cá nhân</div>
-                    <div>Thứ Hai, 31 Tháng 2, 2022</div>
-                    <div>09:00 - 13:00 (4 tiếng)</div>
+                    <div className='fw-bold'>{workingSpaceInfo?.name}</div>
+                    {returnTypeOfBooking(workingSpaceInfo.type) === 'hour' && (
+                      <>
+                        <div>
+                          {moment(orderInfo.orderDetails[0].startDate).format(
+                            'DD/MM/YYYY'
+                          )}
+                        </div>
+                        <div>
+                          {moment(orderInfo.orderDetails[0].startDate).format(
+                            'HH:mm'
+                          )}
+                          {' - '}
+                          {moment(orderInfo.orderDetails[0].endDate).format(
+                            'HH:mm'
+                          )}{' '}
+                          (
+                          {toHoursAndMinutes(
+                            orderInfo.orderDetails[0].hour * 60
+                          )}
+                          )
+                        </div>
+                      </>
+                    )}
+                    {returnTypeOfBooking(workingSpaceInfo.type) === 'day' && (
+                      <>
+                        <div>
+                          {moment(orderInfo.orderDetails[0].startDate).format(
+                            'DD/MM/YYYY'
+                          )}
+                          {' - '}
+                          {moment(orderInfo.orderDetails[0].endDate).format(
+                            'DD/MM/YYYY'
+                          )}
+                        </div>
+                      </>
+                    )}
+                    {/* <div>Thứ Hai, 31 Tháng 2, 2022</div>
+                    <div>09:00 - 13:00 (4 tiếng)</div> */}
                   </div>
                 </div>
               </div>
@@ -232,20 +582,20 @@ export default function BookingPayment() {
               <div className='price-detail'>
                 <div>
                   <span>Giá gốc</span>
-                  <span>250,000đ</span>
+                  <span>{formatCurrency(orderInfo?.total)}</span>
                 </div>
-                <div>
+                {/* <div>
                   <span>Giá sau giảm</span>
                   <span>125,000đ</span>
                 </div>
                 <div>
                   <span>Voucher</span>
                   <span>-25,000đ</span>
-                </div>
+                </div> */}
               </div>
               <div className='price-total'>
                 <span>Tổng cộng</span>
-                <span>100,000đ</span>
+                <span>{formatCurrency(orderInfo?.finalTotal)}</span>
               </div>
               <p className='policy'>
                 Giá trên không bao gồm các chi phí khi bạn sử dụng các dịch vụ
@@ -255,12 +605,20 @@ export default function BookingPayment() {
                 <a href='#'>tại đây</a>
               </p>
             </div>
-            <button type='button' className='payment-button'>
+            <button
+              type='button'
+              className='payment-button'
+              onClick={handleFinish}
+            >
               Tôi đã hoàn tất
             </button>
           </div>
         </div>
       </div>
+      <OrderExpireModal
+        show={showOrderExpire}
+        order_code={orderInfo?.orderId}
+      />
     </div>
   );
 }
