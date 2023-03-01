48ec4dbdd4af22f36f51c9f9719cf58c9e14dc80
htz0707 <nppppppp77@gmail.com>
2023-03-01 11:17:45 +0700

    clean code

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrgqN55Yu90gPGHJbbaWIDvctWYo86cEc",
-  authDomain: "ambient-odyssey-372104.firebaseapp.com",
-  databaseURL: "https://ambient-odyssey-372104-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ambient-odyssey-372104",
-  storageBucket: "ambient-odyssey-372104.appspot.com",
-  messagingSenderId: "988313835315",
-  appId: "1:988313835315:web:43f626eba25003406ad046",
-  measurementId: "G-8XV5P140DN"
+  apiKey: 'AIzaSyDrgqN55Yu90gPGHJbbaWIDvctWYo86cEc',
+  authDomain: 'ambient-odyssey-372104.firebaseapp.com',
+  databaseURL: 'https://ambient-odyssey-372104-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'ambient-odyssey-372104',
+  storageBucket: 'ambient-odyssey-372104.appspot.com',
+  messagingSenderId: '988313835315',
+  appId: '1:988313835315:web:43f626eba25003406ad046',
+  measurementId: 'G-8XV5P140DN'
 };
 
 const app = initializeApp(firebaseConfig);
